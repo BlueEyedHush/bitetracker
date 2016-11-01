3c13c804557c21936b1ce4c7c80bf2e5e06aff1c
shnek94 <shnek94@gmail.com>
2016-11-01 16:00:07 +0100

    Adding cordova app for bitetracker

---
+++ cordova/BiteTracker/platforms/android/assets/www/js/index.js
@@ -0,0 +1,31 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+      window.open = cordova.InAppBrowser.open
+        cordova.InAppBrowser.open('http://bitetracker.herokuapp.com', '_self', 'location=no,zoom=no');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+	navigator.app.loadUrl('http://bitetracker.herokuapp.com', {openExternal:false});
+	console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
