88cd65b9e6516009a8077901b6a7e835b248d134
shnek94 <shnek94@gmail.com>
2016-11-03 17:17:08 +0100

    Clearning project, adding signed apk option

--- cordova/BiteTracker/platforms/android/platform_www/cordova.js
+++ cordova/BiteTracker/platforms/android/platform_www/cordova.js
@@ -8,9 +8,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
      http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '6.0.0';
+var PLATFORM_VERSION_BUILD_LABEL = '4.2.2';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
