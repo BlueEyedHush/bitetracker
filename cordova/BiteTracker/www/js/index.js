8458bfaf084df1318ba0f9aa26f30d6c3d08b503
shnek <shnek94@gmail.com>
2016-12-02 19:03:49 +0100

    Adding basic message for first time app opening without internet

--- cordova/BiteTracker/www/js/index.js
+++ cordova/BiteTracker/www/js/index.js
@@ -24,6 +24,12 @@ var app = {
       inAppBrowser.addEventListener("exit", function(){
         navigator.app.exitApp();
       });
+      inAppBrowser.addEventListener("loaderror", function(event){
+        if(event.message.includes("ERR")){
+          alert("cannot connect to the server")
+          cordova.InAppBrowser.open('./no_internet.html', '_self', 'location=no,zoom=no,hidden=no')
+        }
+      });
       inAppBrowser.addEventListener("loadstop", function(){
         appContainer.show();
       })
